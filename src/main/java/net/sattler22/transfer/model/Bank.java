0a848e0310c62a846d9d6ff6538c7461c8cc97c0
Peter Sattler <peter-sattler@users.noreply.github.com>
2019-01-25 19:18:27 -0500

    Update Bank.java

--- src/main/java/net/sattler22/transfer/model/Bank.java
+++ src/main/java/net/sattler22/transfer/model/Bank.java
@@ -4,10 +4,11 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 /**
- * Revolut&copy; Banking Institution
+ * Revolut Banking Institution Business Object
  *
  * @author Pete Sattler
  * @version January 2019
@@ -36,19 +37,45 @@ public final class Bank implements Serializable {
         return name;
     }
 
+    /**
+     * Get all customers of the bank
+     */
+    public Set<Customer> getCustomers() {
+        return Collections.unmodifiableSet(customers);
+    }
+
+    /**
+     * Add a new customer
+     * 
+     * @return True if the customer was added. Otherwise, returns false when the customer already exists.
+     */
     public boolean addCustomer(Customer customer) {
         return customers.add(customer);
     }
 
     /**
-     * Customer of the bank check
+     * Bank customer existence check
      */
     public boolean isCustomer(Customer customer) {
         return customers.contains(customer);
     }
 
-    public Set<Customer> getCustomers() {
-        return new HashSet<>(customers);  // Defensive copy
+    /**
+     * Delete an existing customer
+     * 
+     * @return True if the customer was deleted. Otherwise, returns false when the customer is non-existent.
+     */
+    public boolean deleteCustomer(Customer customer) {
+        return customers.remove(customer);
+    }
+
+    /**
+     * Find a specific customer
+     * 
+     * @param id The customer identifier
+     */
+    public Optional<Customer> findCustomer(int id) {
+        return customers.stream().filter(customer -> customer.getId() == id).findFirst();
     }
 
     @Override
