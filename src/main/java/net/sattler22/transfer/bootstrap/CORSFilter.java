30cfea724aef194d7abb8d005068bc5c1d5bdfc4
Peter Sattler <peter@sattler22.net>
2019-08-13 23:32:39 -0400

    Set minimal headers based on request type

--- src/main/java/net/sattler22/transfer/bootstrap/CORSFilter.java
+++ src/main/java/net/sattler22/transfer/bootstrap/CORSFilter.java
@@ -3,9 +3,13 @@ package net.sattler22.transfer.bootstrap;
 import java.io.IOException;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Money Transfer Cross-Origin Resource Sharing (CORS) Filter
@@ -13,13 +17,43 @@ import javax.ws.rs.core.MultivaluedMap;
  * @author Pete Sattler
  * @version August 2019
  */
-public final class CORSFilter implements ContainerResponseFilter {
+public final class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CORSFilter.class);
 
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    public void filter(ContainerRequestContext request) throws IOException {
+        if (isPreflightRequest(request)) {
+            LOGGER.info("Aborting CORS preflight request");
+            request.abortWith(Response.ok().build());
+        }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        if (isCrossOriginRequest(request)) {
+            if (isOptionsRequest(request)) {
+                LOGGER.info("CORS preflight request detected");
+                response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+                response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+                response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            }
+            else {
+                LOGGER.info("CORS simple request detected");
+            }
+            response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        }
+    }
+
+    private static boolean isCrossOriginRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null;
+    }
+
+    private static boolean isOptionsRequest(ContainerRequestContext request) {
+        return request.getMethod().equalsIgnoreCase("OPTIONS");
+    }
+
+    private static boolean isPreflightRequest(ContainerRequestContext request) {
+        return isCrossOriginRequest(request) && isOptionsRequest(request);
     }
 }
